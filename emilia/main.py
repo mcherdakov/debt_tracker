78a01b036e6e41c3f65ac540dcc0e19b1ceab44b
mcherdakov <mishokwot@gmail.com>
2020-06-26 02:02:58 +0300

    init

--- emilia/main.py
+++ emilia/main.py
@@ -3,8 +3,8 @@ from handlers import debt_handler, user_handler
 
 app = web.Application()
 app.add_routes([
-    web.get('api/v0/debt', debt_handler),
-    web.get('api/v0/user', user_handler)
+    web.get('/api/v0/debt', debt_handler),
+    web.get('/api/v0/user', user_handler)
 ])
 
 if __name__ == '__main__':
